0963ddffe63198acf6f89ae2ba7c5e6def9b2f65
Noah Lively <noah.lively@koremedia.net>
2011-11-03 07:58:24 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthNoah::Application.config.secret_token = '54e993f4df006c65200da3e1fed38a466afd043d3f7c1cc8631881c8282324bcc43cf65cabaffc117052af8f588134b50ecc7910d02215ec839ec5e0fa0474a7'
